3eb084424867c34883b618b0de3a7bef3c0ec35e
Dmytro Avruschenko <trippyfren@gmail.com>
2023-11-26 18:13:59 +0200

    Add list of authors and quotes, author's details

--- quotesapp/urls.py
+++ quotesapp/urls.py
@@ -5,4 +5,7 @@ app_name = 'quotesapp'
 
 urlpatterns = [
     path('', views.main, name="main"),
+    path('authors/', views.authors_list, name='authors_list'),
+    path('author/<int:author_id>/', views.author_detail, name='author_detail'),
+    path('quotes/', views.quotes_list, name='quotes_list'),
 ]
--- quotesapp/views.py
+++ quotesapp/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Author, Quote
+
 
 # Create your views here.
 def main(request):
     return render(request, 'quotesapp/index.html')
+
+
+def authors_list(request):
+    authors = Author.objects.all()
+    return render(request, 'quotesapp/authors_list.html', {'authors': authors})
+
+
+def author_detail(request, author_id):
+    author = get_object_or_404(Author, pk=author_id)
+    return render(request, 'quotesapp/author_detail.html', {'author': author})
+
+
+def quotes_list(request):
+    quotes = Quote.objects.all()
+    return render(request, 'quotesapp/quotes_list.html', {'quotes': quotes})
