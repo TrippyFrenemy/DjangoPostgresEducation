ebe85b310451033f6a5ba5454983844360bcb6a3
Dmytro Avruschenko <trippyfren@gmail.com>
2023-11-26 17:25:13 +0200

    Add lodout

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .forms import RegisterForm, LoginForm
 
@@ -33,3 +34,9 @@ def loginuser(request):
         return redirect(to='quotesapp:main')
 
     return render(request, 'users/login.html', context={"form": LoginForm()})
+
+
+@login_required
+def logoutuser(request):
+    logout(request)
+    return redirect(to='quotesapp:main')
