82c2a2c8a3a5fefb1a07904f785f96ba76df2adc
Dmytro Avruschenko <trippyfren@gmail.com>
2023-11-26 17:16:31 +0200

    Add models

--- quotesapp/models.py
+++ quotesapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    fullname = models.CharField(max_length=100)
+    born_date = models.CharField(max_length=50)
+    born_location = models.TextField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.fullname
+
+
+class Quote(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.PROTECT)
+    quote = models.TextField()
+    tags = models.JSONField()
+
+    def __str__(self):
+        return f"Quote by {self.author.fullname}"
