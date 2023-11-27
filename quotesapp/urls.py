a57f96ab84fd084c9898149c470674061b5fdfe0
Dmytro Avruschenko <trippyfren@gmail.com>
2023-11-27 21:33:03 +0200

    Add deleting quotes

--- quotesapp/urls.py
+++ quotesapp/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('authors/', views.authors_list, name='authors_list'),
     path('author/<int:author_id>/', views.author_detail, name='author_detail'),
     path('quotes/', views.quotes_list, name='quotes_list'),
+    path('add_author/', views.add_author, name='add_author'),
+    path('add_quote/', views.add_quote, name='add_quote'),
+    path('quotes/delete/<int:quote_id>/', views.delete_quote, name='delete_quote'),
 ]
--- quotesapp/views.py
+++ quotesapp/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Author, Quote
+from .forms import AuthorForm, QuoteForm
+
 
 
 # Create your views here.
@@ -21,3 +24,42 @@ def author_detail(request, author_id):
 def quotes_list(request):
     quotes = Quote.objects.all()
     return render(request, 'quotesapp/quotes_list.html', {'quotes': quotes})
+
+
+@login_required
+def add_author(request):
+    if request.method == 'POST':
+        form = AuthorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/authors')
+    else:
+        form = AuthorForm()
+    return render(request, 'quotesapp/add_author.html', {'form': form})
+
+
+@login_required
+def add_quote(request):
+    if request.method == 'POST':
+        form = QuoteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/quotes")
+    else:
+        form = QuoteForm()
+
+    return render(request, 'quotesapp/add_quote.html', {'form': form})
+
+
+@login_required
+def delete_quote(request, quote_id):
+    quote = get_object_or_404(Quote, id=quote_id)
+
+    # Перевірка, що користувач має права на видалення цитати
+    # Можна додати додаткові умови, наприклад, перевірку, чи користувач є автором цитати
+
+    if request.method == 'POST':
+        quote.delete()
+        return redirect('/quotes')
+
+    return render(request, 'quotesapp/delete_quote_confirm.html', {'quote': quote})
